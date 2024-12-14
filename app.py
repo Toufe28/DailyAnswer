531a1cbe55170fabf1b9afc49e7285cbcfcd8128
Gangster <40878621+GangsterGpc@users.noreply.github.com>
2024-12-14 17:50:49 +0630

    test app.py

--- app.py
+++ app.py
@@ -1,46 +1,45 @@
 
 def get_price_prediction_answer(card_id):
     answers = {
-    "7814": "51000",
-    "7816": "265000",
-    "7812": "43000",
-    "7813": "10000",
-    "7817": "22000",
-    "7815": "13000",
-    "7819": "55000",
-    "7818": "13000",
-    "7821": "15000",
-    "7820": "239000"
+    "7762": "99000",
+    "7763": "52000",
+    "7770": "117000",
+    "7771": "31000",
+    "7765": "14000",
+    "7769": "38000",
+    "7764": "10000",
+    "7767": "277000",
+    "7768": "12000",
+    "7766": "303000"
 }
     return answers.get(str(card_id), None)
 
 def get_who_is_the_artist_answer(card_id):
     answers = {
-    "7805": "Kim WhanKi",
-    "7802": "Pu Ru",
-    "7807": "Alexander Calder",
-    "7806": "Josef Albers",
-    "7804": "Xu Beihong",
-    "7803": "Camille Pissarro",
-    "7809": "Qi Baishi",
-    "7808": "Zhang Daqian",
-    "7811": "Takashi Murakami",
-    "7810": "Wu Guanzhong"
+    "7752": "Maurice de Vlaminck",
+    "7755": "Pu Ru",
+    "7753": "Sanyu",
+    "7754": "Andy Warhol",
+    "7756": "Lin Fengmian",
+    "7757": "Qi Baishi",
+    "7759": "Pierre Bonnard",
+    "7758": "Zhang Daqian",
+    "7760": "David Hockney",
+    "7761": "Camille Pissarro"
 }
     return answers.get(str(card_id), None)
 
 def get_artist_market_value_answer(card_id):
     answers = {
-    "7795": "$622.9 million",
-    "7793": "$1.0 billion",
-    "7792": "$444.9 million",
-    "7801": "$851.2 million",
-    "7794": "$551.0 million",
-    "7796": "$1.4 billion",
-    "7798": "$315.0 million",
-    "7797": "$1.9 billion",
-    "7799": "$205.6 million",
-    "7800": "$1.0 billion"
+    "7742": "$1.5 billion",
+    "7743": "$8.6 billion",
+    "7746": "$16.0 billion",
+    "7748": "$205.6 million",
+    "7745": "$2.1 billion",
+    "7749": "$1.0 billion",
+    "7750": "$798.9 million",
+    "7751": "$330.8 million",
+    "7744": "$851.2 million",
+    "7747": "$482.0 million"
 }
     return answers.get(str(card_id), None)
-
