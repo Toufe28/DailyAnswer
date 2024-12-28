38ef82f54bf72b6d41cae7ff17ee3beac1166494
Gangster <40878621+GangsterGpc@users.noreply.github.com>
2024-12-28 17:22:43 +0630

    28.12.2024 update answers.py

--- answers.py
+++ answers.py
@@ -1,45 +1,45 @@
 
 def get_price_prediction_answer(card_id):
     answers = {
-    "8470": "34000",
-    "8462": "29000",
-    "8471": "23000",
-    "8466": "253000",
-    "8463": "17000",
-    "8464": "241000",
-    "8465": "13000",
-    "8467": "26000",
-    "8468": "12000",
-    "8469": "187000"
+    "8512": "47000",
+    "8513": "33000",
+    "8515": "11000",
+    "8514": "13000",
+    "8519": "23000",
+    "8516": "61000",
+    "8517": "22000",
+    "8518": "45000",
+    "8520": "113000",
+    "8521": "22000"
 }
     return answers.get(str(card_id), None)
 
 def get_who_is_the_artist_answer(card_id):
     answers = {
-    "8452": "Pu Ru",
-    "8453": "Qi Baishi",
-    "8455": "Fernand Leger",
-    "8457": "Keith Haring",
-    "8454": "Lin Fengmian",
-    "8456": "Damien Hirst",
-    "8458": "Peter Doig",
-    "8459": "Jonas Wood",
-    "8460": "Alex Katz",
-    "8461": "George Condo"
+    "8502": "Qi Baishi",
+    "8505": "Kaws",
+    "8504": "John Currin",
+    "8503": "Pu Ru",
+    "8506": "Peter Doig",
+    "8508": "Julie Mehretu",
+    "8507": "Lee Ufan",
+    "8510": "Thomas Sch\u00fctte",
+    "8509": "Damien Hirst",
+    "8511": "Alex Katz"
 }
     return answers.get(str(card_id), None)
 
 def get_artist_market_value_answer(card_id):
     answers = {
-    "8444": "$1.0 billion",
-    "8442": "$851.3 million",
-    "8443": "$205.6 million",
-    "8445": "$460.1 million",
-    "8448": "$825.7 million",
-    "8446": "$552.6 million",
-    "8449": "$799.1 million",
-    "8450": "$551.3 million",
-    "8447": "$1.3 billion",
-    "8451": "$296.0 million"
+    "8493": "$205.6 million",
+    "8492": "$851.3 million",
+    "8494": "$460.1 million",
+    "8495": "$472.8 million",
+    "8498": "$1.0 billion",
+    "8496": "$825.7 million",
+    "8500": "$1.0 billion",
+    "8497": "$16.0 billion",
+    "8499": "$552.6 million",
+    "8501": "$642.4 million"
 }
     return answers.get(str(card_id), None)
